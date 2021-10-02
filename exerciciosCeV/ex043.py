17482c089cb0acbeb1917afed829d3969356e3b7
Rian Carlos <88385231+riancarlos04@users.noreply.github.com>
2021-10-02 09:34:17 -0300

    043 and 045

--- exerciciosCeV/ex043.py
+++ exerciciosCeV/ex043.py
@@ -1,1 +1,17 @@
-IMC
+from math import pow
+print('-='*20)
+print('CALCULADORA DE IMC')
+print('-='*20)
+altura = float(input('Digite a sua altura (M) : '))
+peso = float(input('Digite o seu peso (Kg): '))
+IMC = peso / (pow(altura, 2))
+if IMC < 18.5:
+    print(f'Seu IMC é de {IMC:.2f}, VOCE ESTA ABAIXO DO PESO')
+elif IMC >= 18.5 and IMC < 25:
+    print(f'Seu IMC é de {IMC:.2f}, VOCE ESTA NO PESO IDEAL')
+elif IMC >= 25 and IMC < 30:
+    print(f'Seu IMC é de {IMC:.2f}, VOCE ESTA COM SOBREPESO')
+elif IMC >= 30 and IMC < 40:
+    print(f'Seu IMC é de {IMC:.2f}, VOCE COM OBESIDADE')
+else:
+    print(f'Seu IMC é de {IMC:.2f}, VOCE ESTA COM OBESIDADE MORBIDA')
--- exerciciosCeV/ex045.py
+++ exerciciosCeV/ex045.py
@@ -1,1 +1,22 @@
-jogo advimnhacao 2.0
+from random import choice
+print('-='*20)
+print('PEDRA , PAPEL OU TESOURA')
+print('-='*20)
+lista = ['PEDRA', 'PAPEL', 'TESOURA']
+jogador = str(input('Faca uma jogada (PEDRA, PAPEL OU TESOURA): ')).strip().upper()
+computador = choice(lista)
+if jogador == 'PEDRA' and computador == 'TESOURA':
+    print('Voce ganhou!, computador jogou TESOURA')
+elif jogador == 'TESOURA' and computador == 'PEDRA':
+    print('Voce Perdeu!, computador jogou PEDRA')
+elif jogador == 'PAPEL' and computador == 'PEDRA':
+    print('Voce ganhou!, computador jogou PEDRA')
+elif jogador == 'PEDRA' and computador == 'PAPEL':
+    print('Voce Perdeu!, computador jogou PAPEL')
+elif jogador == 'TESOURA' and computador == 'PAPEL':
+    print('Voce ganhou, computador jogou PAPEL')
+elif jogador == 'PAPEL' and computador == 'TESOURA':
+    print('Voce perdeu!, computador jogou TESOURA')
+elif jogador == computador:
+    print(f'EMPATE, computador jogou {computador}')
+
