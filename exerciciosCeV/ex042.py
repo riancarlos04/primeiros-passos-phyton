5e57f6ffd2ca34a29434e893db378cebbe21bfa4
Rian Carlos <88385231+riancarlos04@users.noreply.github.com>
2021-10-06 10:09:59 -0300

    correção 042 to 044

--- exerciciosCeV/ex042.py
+++ exerciciosCeV/ex042.py
@@ -1,12 +1,14 @@
 s1 = float(input('Digite o seguimento 1: '))
 s2 = float(input('Digite o seguimento 2: '))
 s3 = float(input('Digite o seguimento 3:'))
-if s1 < (s2 + s3) and s2 < (s1 + s3) and s3 < (s1 + s2) and s1 == s2 and s2 == s3:
-    print(f'Com os seguimentos acima pode se formar um triangulo EQUILÁTERO ')
-elif 1 < (s2 + s3) and s2 < (s1 + s3) and s3 < (s1 + s2) and s1 == s2 and s3 != s1 or s2 == s3 and s1 != s3 or s3 == s1 and s2 != s1:
-    print(f'Com os seguimentos acima pode se formar um triangulo ISOCELES ')
-elif s1 < (s2 + s3) and s2 < (s1 + s3) and s3 < (s1 + s2) and s1 != s2 and s2 != s3 and s3 != s1:
-    print(f'Com os seguimentos acima pode se formar um triangulo ESCALENO ')
+if s1 < (s2 + s3) and s2 < (s1 + s3) and s3 < (s1 + s2):
+    print(f'Com os seguimentos acima pode se formar um triangulo ', end='')
+    if s1 == s2 == s3:
+        print('EQUILÁTERO!')
+    elif s1 != s2 != s3 != s1:
+        print('ESCALENO!')
+    else:
+        print('ISÓCELES')
+
 else:
     print('Com os seguimentos acima NÃo se pode formar um triangulo')
-ç
--- exerciciosCeV/ex044.py
+++ exerciciosCeV/ex044.py
@@ -1,1 +1,23 @@
-valor produto
+produto = float(input('Digite o valor do produto R$ '))
+print('''Opçoes de pagamento:
+[ 1 ] A vista (dinheiro ou cheque)
+[ 2 ] A vista no cartao
+[ 3 ] 2x no cartao
+[ 4 ] 3x ou mais no cartao ''')
+opcao = int(input('Digite a opção: '))
+if opcao == 1:
+    valorfinal = produto - (produto * 10 / 100)
+    print(f'O valor final será de R$ {valorfinal:.2f}')
+elif opcao == 2:
+    valorfinal = produto - (produto * 5 / 100)
+    print(f'O valor final sera de R$ {valorfinal:.2f}')
+elif opcao == 3:
+    parcela = produto / 2
+    print(f'O valor da parcela é de R${parcela:.2f} e o valor final do produto é R$ {produto:.2f}')
+elif opcao == 4:
+    numparcela = int(input('Digite o número de parcelas: '))
+    valorfinal = produto + (produto * 20 / 100)
+    parcela = valorfinal / numparcela
+    print(f'O valor da parcela sera de R$ {parcela:.2f} e o valor final sera de R$ {valorfinal:.2f}')
+else:
+    print('Opção invalida')
