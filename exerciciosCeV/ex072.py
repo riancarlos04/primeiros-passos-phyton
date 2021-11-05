2cefa9a57b7662afed1ece90c4d20e37ce16d6a5
Rian Carlos <88385231+riancarlos04@users.noreply.github.com>
2021-11-05 08:53:24 -0300

    Update ex072.py

--- exerciciosCeV/ex072.py
+++ exerciciosCeV/ex072.py
@@ -1,3 +1,8 @@
-num = 8
-while num >= 0 and num <= 20:
-    num = int(input('Digite um numero entre 0 e 20: '))
+extenso = 'Zero', 'Um', 'Dois', 'Tres', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatoze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
+while True:
+    num = int(input('Digite um numero entre 0 e 20 para ser escrito por extenso: '))
+    while num < 0 and num > 20:
+        num = int(input('Opção Inválida. Digite um numero entre 0 e 20 para ser escrito por extenso: '))
+    if num >= 0 and num <= 20:
+        break
+print(f'O numero {num} por extenso é: {extenso[num]}')
